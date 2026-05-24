bad34c495b1f0f4ee68ff978f5758420ee7d7e60
Vishweshwar Reddy <vishweshwarreddy1718@gmail.com>
2026-05-24 16:13:17 +0530

    added render url

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:5000";
+const SOCKET_URL = "https://ipl-trump-game.onrender.com";
 
 if (process.env.NODE_ENV === "development") {
   console.log("Connecting to:", SOCKET_URL);
